b2f43b389b532682056dbd74e5432f8499aa1892
Andres Ramirez <andreskmilorr97@gmail.com>
2024-02-21 13:54:25 -0500

    add project to git

---
+++ API_Rick_and_Morty_hubspot/src/routes/characters.js
@@ -0,0 +1,113 @@
+const {Router} = require('express');
+const router = Router();
+const _ = require('underscore');
+const fetch = require('node-fetch');
+const { route } = require('.');
+const hubspot = require('@hubspot/api-client');
+const hubspotClient = new hubspot.Client({ accessToken: 'pat-na1-39ec74a2-7552-4bef-a4d2-9c65301ba2f3' });
+
+let url_character = "https://rickandmortyapi.com/api/character";
+
+async function createContac(characters){
+    const data = characters.map(async(character)=>{
+        const contact = {
+            properties:{
+                "character_id": character.id,
+                "firstname": character.firstname,
+                "lastname": character.lastname,
+                "status_character": character.status_character,
+                "character_species": character.character_species,
+                "character_gender": character.character_gender,
+            },
+        }
+        console.log(contact);
+        console.log(character);
+        // return contact
+        const createContactResponse = await hubspotClient.crm.contacts.basicApi.create(contact);
+    });
+}
+
+async function functionContacts(url) {
+    const response = await fetch(url);
+    const characters = await response.json();
+
+    const data = characters.results.map((character)=>{
+        const fullName = character.name.split(" ");
+        let firstName = "";
+        let lastname = "";
+        for (let i in fullName){
+            if(i == (fullName.length - 1)){
+                lastname = fullName[i];
+            }else{
+                firstName = firstName + fullName[i] + " ";
+            }
+        } 
+        return {
+            "id": character.id,
+            "firstname": firstName.trim(),
+            "lastname": lastname,
+            "status_character": character.status,
+            "character_species": character.species,
+            "character_gender": character.gender
+        } 
+    });
+    return data;
+}
+
+let contacts; 
+functionContacts(url_character).then(data => contacts = data);
+
+
+router.get('/', async (req,res)=>{   
+    res.send(contacts);
+    createContac(contacts);
+}); 
+
+router.post('/',(req,res)=>{
+    console.log("POST");
+    const { firstname, lastname, status_character, character_species, character_gender }=req.body;
+    if (firstname&& lastname && status_character && character_species && character_gender){
+        const id = contacts.length + 1;
+        const newCharacter = {id,...req.body};
+        // contacts.push(newCharacter);
+        console.log(newCharacter);
+        // res.json(contacts);
+    }else{
+        res.status(500).json({error: 'There was an error.'});
+    }
+    console.log("\nreq :\n",req.body);
+    res.send('received');
+});
+
+router.put('/:id', (req,res)=>{
+    const {id}=req.params;
+    const { firstname, lastname, status_character, character_species, character_gender } = req.body;
+    if(firstname&& lastname && status_character && character_species && character_gender){
+        _.each(contacts,(contact,i)=>{
+            if(contact.id == id){
+                contact.firstname = firstname;
+                contact.lastname = lastname;
+                contact.status_character = status_character;
+                contact.character_species = character_species;
+                contact.character_gender = character_gender;
+                // console.log(firstname)
+            }
+        });
+        res.json(contacts);
+    }else{
+        res.status(500).json({error: 'There was an error.'})
+    }
+});
+
+router.delete('/:id', (req,res)=>{
+    const {id} = req.params;
+    _.each(contacts, (contact, i) => {
+        if (contact.id == id){
+            contacts.splice(i, 1);
+        }
+    })
+    res.send(contacts);
+});
+
+
+module.exports = router;
---
+++ API_Rick_and_Morty_hubspot/src/routes/locations.js
@@ -0,0 +1,97 @@
+const {Router} = require('express');
+const router = Router();
+const _ = require('underscore');
+const fetch = require('node-fetch');
+const { route } = require('.');
+const hubspot = require('@hubspot/api-client');
+const hubspotClient = new hubspot.Client({ accessToken: 'pat-na1-39ec74a2-7552-4bef-a4d2-9c65301ba2f3' });
+
+
+let url_location = "https://rickandmortyapi.com/api/location"
+
+async function createCompany(companies){
+    const data = companies.map(async(company)=>{
+        const location = {
+            properties:{
+                "location_id": company.id,
+                "name": company.name,
+                "location_type": company.location_type,
+                "dimension": company.dimension,
+                "creation_date": company.creation_date,
+            },
+        }
+        console.log("\nlocation \n",location);
+        console.log("\nCompany \n",company,"\n\n");
+        const createCompanyResponse = await hubspotClient.crm.companies.basicApi.create(location);
+    });
+}
+
+async function functionLocations(url) {
+    const response = await fetch(url);
+    const locations = await response.json();
+
+    const data = locations.results.map((location)=>{
+        return {
+            "id": location.id,
+            "name": location.name,
+            "location_type": location.type,
+            "dimension": location.dimension,
+            "creation_date": location.created
+        } 
+    });
+    return data;
+}
+
+let locations; 
+functionLocations(url_location).then(data => locations = data);
+
+
+router.get('/', async (req,res)=>{
+    res.send(locations);
+    createCompany(locations);
+}); 
+
+router.post('/',(req,res)=>{
+    const { name, location_type, dimension, creation_date }=req.body;
+    if (name && location_type && dimension && creation_date){
+        const id = locations.length + 1;
+        const newLocation = {id,...req.body};
+        // locations.push(newLocation);
+        // res.json(locations);
+        console.log(newLocation);
+    }else{
+        res.status(500).json({error: 'There was an error.'});
+    }
+    console.log("\nreq :\n",req.body);
+    res.send('received');
+});
+
+router.put('/:id', (req,res)=>{
+    const {id}=req.params;
+    const { name, location_type, dimension, creation_date } = req.body;
+    if(name && location_type && dimension && creation_date){
+        _.each(locations,(location,i)=>{
+            if(location.id == id){
+                location.name = name;
+                location.location_type = location_type;
+                location.dimension = dimension;
+                location.creation_date = creation_date;
+            }
+        });
+        res.json(locations);
+    }else{
+        res.status(500).json({error: 'There was an error.'})
+    }
+});
+
+router.delete('/:id', (req,res)=>{
+    const {id} = req.params;
+    _.each(locations, (location, i) => {
+        if (location.id == id){
+            location.splice(i, 1);
+        }
+    })
+    res.send(locations);
+});
+
+module.exports = router;
