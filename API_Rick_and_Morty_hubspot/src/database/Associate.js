7f5a2c41aec20cce27f1aa9a5c181840ec1aec8e
Andres Ramirez <andreskmilorr97@gmail.com>
2024-02-23 20:07:12 -0500

    update associate

--- API_Rick_and_Morty_hubspot/src/database/Associate.js
+++ API_Rick_and_Morty_hubspot/src/database/Associate.js
@@ -15,38 +15,50 @@ async function associationContactCompany() {
     const allCompanies = await hubspotClient.crm.companies.getAll(undefined, undefined, ["name", "location_id"]);
     const allContacts = await hubspotClient.crm.contacts.getAll(undefined, undefined, ["firstname", "lastname", "country"]);
     const allAssociates = [];
-    allContacts.map(async (contact) => {
-        allCompanies.map(async (company) => {
-            if (contact.properties.country === company.properties.name) {
-                const associate = {
-                    "assocaites": {
-                        "contact": {
-                            "name": contact.properties.firstname + ' ' + contact.properties.lastname
-                        },
-                        "company": {
-                            "name": company.properties.name
+
+    for (let i in allContacts) {
+        const contact = allContacts[i];
+        const companiesAssociateContact = await hubspotClient.crm.associations.v4.basicApi.getPage(
+            'contact',
+            contact.id,
+            'company',
+        );
+        if (!companiesAssociateContact.results[0]) {
+
+            for (let j in allCompanies) {
+                const company = allCompanies[j]
+                if (contact.properties.country === company.properties.name) {
+                        const associate = {
+                            "assocaites": {
+                                "contact": {
+                                    "name": contact.properties.firstname + ' ' + contact.properties.lastname
+                                },
+                                "company": {
+                                    "name": company.properties.name
+                                }
+                            }
                         }
-                    }
+                        console.log("\nDatabase New Associate\n");
+                        console.log(associate);
+                        allAssociates.push(associate);
+                        const createAssociation = await hubspotClient.crm.associations.v4.basicApi.create(
+                            'companies',
+                            company.id,
+                            'contacts',
+                            contact.id,
+                            [
+                                {
+                                    "associationCategory": "HUBSPOT_DEFINED",
+                                    "associationTypeId": 2
+                                    // AssociationTypes contains the most popular HubSpot defined association types
+                                }
+                            ]
+                        );
+                    // }
                 }
-                allAssociates.push(associate);
-                const createAssociation = await hubspotClient.crm.associations.v4.basicApi.create(
-                    'companies',
-                    company.id,
-                    'contacts',
-                    contact.id,
-                    [
-                        {
-                            "associationCategory": "HUBSPOT_DEFINED",
-                            "associationTypeId": 2
-                            // AssociationTypes contains the most popular HubSpot defined association types
-                        }
-                    ]
-                );
             }
-        });
-    });
-    console.log("\n Data Base\n")
-    console.log(allAssociates)
+        }
+    }
     return allAssociates;
 }
 
