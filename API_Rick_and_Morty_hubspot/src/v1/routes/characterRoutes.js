839bfa67f298399398bdcddd34760202b9ccb926
Andres Ramirez <andreskmilorr97@gmail.com>
2024-02-25 12:06:58 -0500

    update

--- API_Rick_and_Morty_hubspot/src/v1/routes/characterRoutes.js
+++ API_Rick_and_Morty_hubspot/src/v1/routes/characterRoutes.js
@@ -1,5 +1,4 @@
 const { Router } = require("express");
-const _ = require("underscore");
 const characterController = require("../../controllers/characterController");
 const router = Router();
 
@@ -7,35 +6,5 @@ router
   .get("/", characterController.getAllCharacters)
   .get("/create", characterController.getCreateContact)
   .post("/", characterController.postCreateUpdateContact);
-
-// router.put('/:id', (req, res) => {
-//     const { id } = req.params;
-//     const { firstname, lastname, status_character, character_species, character_gender } = req.body;
-//     if (firstname && lastname && status_character && character_species && character_gender) {
-//         _.each(contacts, (contact, i) => {
-//             if (contact.id == id) {
-//                 contact.firstname = firstname;
-//                 contact.lastname = lastname;
-//                 contact.status_character = status_character;
-//                 contact.character_species = character_species;
-//                 contact.character_gender = character_gender;
-//                 // console.log(firstname)
-//             }
-//         });
-//         res.json(contacts);
-//     } else {
-//         res.status(500).json({ error: 'There was an error.' })
-//     }
-// });
-
-// router.delete('/:id', (req, res) => {
-//     const { id } = req.params;
-//     _.each(contacts, (contact, i) => {
-//         if (contact.id == id) {
-//             contacts.splice(i, 1);
-//         }
-//     })
-//     res.send(contacts);
-// });
 
 module.exports = router;
--- API_Rick_and_Morty_hubspot/src/v1/routes/locationRoutes.js
+++ API_Rick_and_Morty_hubspot/src/v1/routes/locationRoutes.js
@@ -1,40 +1,10 @@
 const { Router } = require("express");
-const _ = require("underscore");
 const locationController = require("../../controllers/locationController");
-const { route } = require("./characterRoutes");
 const router = Router();
 
 router
   .get("/", locationController.getAllLocations)
   .get("/create", locationController.getCreateCompany)
   .post("/", locationController.postCreateUpdateCompany);
-
-// router.put('/:id', (req, res) => {
-//     const { id } = req.params;
-//     const { name, location_type, dimension, creation_date } = req.body;
-//     if (name && location_type && dimension && creation_date) {
-//         _.each(locations, (location, i) => {
-//             if (location.id == id) {
-//                 location.name = name;
-//                 location.location_type = location_type;
-//                 location.dimension = dimension;
-//                 location.creation_date = creation_date;
-//             }
-//         });
-//         res.json(locations);
-//     } else {
-//         res.status(500).json({ error: 'There was an error.' })
-//     }
-// });
-
-// router.delete('/:id', (req, res) => {
-//     const { id } = req.params;
-//     _.each(locations, (location, i) => {
-//         if (location.id == id) {
-//             location.splice(i, 1);
-//         }
-//     })
-//     res.send(locations);
-// });
 
 module.exports = router;
